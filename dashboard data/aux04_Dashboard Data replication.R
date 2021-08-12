33ed73b619976c14bca01f2f0e793e65ba4a20f7
jvargh7 <jvargh7@emory.edu>
2021-08-12 12:44:52 -0400

    Added aux04_ to replace SAS script

---
+++ dashboard data/aux04_Dashboard Data replication.R
@@ -0,0 +1,56 @@
+
+current_date = "2021-08-08"
+
+# up to line 500 - Covidtimeseries_pop ---------------
+
+view_data = FALSE # Used in dd03_covidtimeseries00.R 
+source(paste0(path_c19dashboard_repo,"/dashboard data/dd01_nytimes.R"))
+source(paste0(path_c19dashboard_repo,"/dashboard data/dd03_covidtimeseries00.R"))
+rm(covidtimeseries,county_daily,nyt_counties)
+
+# From dd02_urbancodes.R ---------
+urbancodes3 <- readRDS(paste0(path_c19dashboard_shared_folder,"/Data/Processed/CDC_Urban_Rural/urbancodes3.RDS"))
+
+
+covidtimeseries_pop <- covidtimeseries_pop  %>% 
+  rename_at(vars(matches("_fig")),function(x) str_replace(x,"_fig","fig")) %>%
+  left_join(urbancodes3 %>% 
+              dplyr::select(state,county,Urbanization_2013,Urbanization_Code_2013,urbanrural,
+                            annualdeaths2018,annualmortality2018,crude_rate),
+            by = c("state","county"))  %>% 
+  dplyr::rename(
+    # countyname = county_name,
+    # countycode = county_code,
+    # CHECK Are these being used ---------
+    X_2013_Urbanization = Urbanization_2013,
+    X_2013_Urbanization_Code = Urbanization_Code_2013,
+    Population = population,
+    Crude_Rate = crude_rate
+  )
+
+saveRDS(covidtimeseries_pop,paste0(path_c19dashboard_shared_folder,"/Data/Processed/NYT Covid19 data/covidtimeseries_pop.RDS"))
+write.csv(head(covidtimeseries_pop,n=1000),paste0(path_c19dashboard_shared_folder,"/Data/Processed/NYT Covid19 data/EXAMPLE_covidtimeseries_pop.csv"),row.names = FALSE)
+
+
+# up to line 789 - mergedsocial -------------
+# covidtimeseries_pop <- readRDS(paste0(path_c19dashboard_shared_folder,"/Data/Processed/NYT Covid19 data/covidtimeseries_pop.RDS"))
+
+source(paste0(path_c19dashboard_repo,"/dashboard data/dd08_mergedsocial.R"))
+
+mean7daycurrent <- covidtimeseries_pop %>% 
+  dplyr::filter(date == current_date)
+
+mergedsocial <- mergedsocial %>% 
+  left_join(mean7daycurrent,
+            by=c("state","county","nation"))
+
+# SAVE ---------
+
+saveRDS(mergedsocial,paste0(path_c19dashboard_shared_folder,"/Data/Processed/mergedsocial/mergedsocial.RDS"))
+haven::write_sas(mergedsocial,paste0(path_c19dashboard_shared_folder,"/Data/Processed/mergedsocial/mergedsocial.sas7bdat"))
+write.csv(mergedsocial,paste0(path_c19dashboard_shared_folder,"/Data/Processed/mergedsocial/mergedsocial.csv"),row.names = FALSE)
+write.csv(head(mergedsocial,n=1000),paste0(path_c19dashboard_shared_folder,"/Data/Processed/mergedsocial/EXAMPLE_mergedsocial.csv"),row.names = FALSE)
+
+
+# Missing columns in mergedsocial ------
+# [1] "X1"          "VAR1"        "County_Code" "countyname"  "countynum"   "countycode"  "statecode"   "statename"   "zero"   
